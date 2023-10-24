0f4b4bdfa62e2d907b53e87ca14be0a145bb9622
alexraduca01 <alex.raduca01@icloud.com>
2023-10-24 14:24:01 +0200

    completed snack 2

--- script.js
+++ script.js
@@ -37,35 +37,53 @@ const students = [
         id: 213,
         name: "Marco della Rovere",
         grades: 78,
-    }
+    },
     {
         id: 110,
         name: "Paola Cortellessa",
-        grades: 78,
-    }
+        grades: 96,
+    },
     {
-        id: 213,
-        name: "Marco della Rovere",
-        grades: 78,
-    }
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48,
+    },
     {
-        id: 213,
-        name: "Marco della Rovere",
-        grades: 78,
-    }
+        id: 145,
+        name: "Gaia Borromini",
+        grades: 74,
+    },
     {
-        id: 213,
-        name: "Marco della Rovere",
-        grades: 78,
-    }
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68,
+    },
     {
-        id: 213,
-        name: "Marco della Rovere",
-        grades: 78,
-    }
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50,
+    },
     {
-        id: 213,
-        name: "Marco della Rovere",
-        grades: 78,
-    }
-]
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84,
+    },
+];
+
+const aula = students.map((studente) => {
+    return studente.name.toUpperCase();
+})
+
+console.log(aula);
+
+const over70 = students.filter((studente) => {
+    return studente.grades > 70;
+})
+
+console.log(over70);
+
+const superior120 = students.filter((studente) => {
+    return studente.grades > 70 && studente.id > 120;
+})
+
+console.log(superior120);
