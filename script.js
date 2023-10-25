18b7364cc1551d41766cbca95eb2906cd9759da2
alexraduca01 <alex.raduca01@icloud.com>
2023-10-25 15:20:49 +0200

    completed snack 4

--- script.js
+++ script.js
@@ -127,3 +127,48 @@ const result = depositoBici.filter((bici) => {
     return bici.nome && bici.peso === Math.min(...minor);
 })
 console.log(`la bici che pesa meno è: ${result[0].nome} e pesa: ${result[0].peso}kg`);
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // SNACK 4 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const serieA = [
+    {
+        nome: "Inter",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Milan",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Lazio",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Napoli",
+        punti: 0,
+        falliSubiti: 0,
+    },
+];
+
+serieA.forEach((el, index) =>{
+    const copySerieA = {...serieA[index], punti: getRndInteger(1, 100), falliSubiti: getRndInteger(1, 100)};
+    console.log(copySerieA);
+    const {nome, falliSubiti} = copySerieA;
+    console.log({nome, falliSubiti});
+})
